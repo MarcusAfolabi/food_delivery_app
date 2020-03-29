e78befaf1ddbf5b9742cfa1b0783173888adabaf
MarcusBliss <biormarcus@gmail.com>
2020-03-29 10:04:09 +0100

    Initial commit

---
+++ lib/src/data/category_data.dart
@@ -0,0 +1,54 @@
+import '../models/category_model.dart';
+
+final categories = [
+ Category(
+   numberOfItems: 43,
+   imagePath: "assets/images/burger.png",
+   categoryName: "Burger"
+ ),
+ Category(
+     numberOfItems: 33,
+     imagePath: "assets/images/pizza.png",
+     categoryName: "Pizza"
+ ),
+ Category(
+     numberOfItems: 23,
+     imagePath: "assets/images/beer.png",
+     categoryName: "Beer"
+ ),
+ Category(
+     numberOfItems: 13,
+     imagePath: "assets/images/breakfast.jpeg",
+     categoryName: "Breakfast"
+ ),
+ Category(
+     numberOfItems: 20,
+     imagePath: "assets/images/cheeseburger.png",
+     categoryName: "Cheeseburger"
+ ),
+ Category(
+     numberOfItems: 20,
+     imagePath: "assets/images/coffee-cup.png",
+     categoryName: "Coffee-Cup"
+ ),
+ Category(
+     numberOfItems: 40,
+     imagePath: "assets/images/cupcake.png",
+     categoryName: "Cupcake"
+ ),
+ Category(
+     numberOfItems: 54,
+     imagePath: "assets/images/lunch.jpeg",
+     categoryName: "Lunch"
+ ),
+ Category(
+     numberOfItems: 54,
+     imagePath: "assets/images/supper_1.jpeg",
+     categoryName: "Supper"
+ ),
+ Category(
+     numberOfItems: 54,
+     imagePath: "assets/images/turkey.png",
+     categoryName: "Turkey"
+ ),
+];
---
+++ lib/src/data/food_data.dart
@@ -0,0 +1,21 @@
+import '../models/food_model.dart';
+    final foods = [
+      Food(
+        id: "1",
+        name: "Hot Coffee",
+        imagePath: "assets/images/breakfast.jpeg",
+        category: "1",
+        price: 22.0,
+        discount: 35.5,
+        ratings: 99.0,
+      ),
+      Food(
+        id: "1",
+        name: "Drilled Chicken",
+        imagePath: "assets/images/lunch.jpeg",
+        category: "2",
+        price: 12.0,
+        discount: 31.5,
+        ratings: 89.0,
+      ),
+    ];
